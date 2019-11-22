659c4be8112ccbc5817e0836babd3161a04c20f3
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-22 01:10:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+
+def starts_with_a_vowel?(word)
+word.match(/\A[aeiouAEIOU]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un\w*ing/)
+binding.pry
 end
 
 def words_five_letters_long(text)
-
+text.scan(/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
